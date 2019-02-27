c44a0ed590fa2617af156fae94e1505de0f13db5
Owen Daughtery <OwenJ.daughtery@gmail.com>
2019-02-27 12:44:11 +0000

    initial commit

---
+++ Assets/SteamVR_Input/ViveInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+public class ViveInput : MonoBehaviour
+{
+
+    void Update(){
+        if (SteamVR_Actions.default_GrabPinch.GetStateUp(SteamVR_Input_Sources.Any)) {
+            print("trigger has been released");
+        }
+    }
+}
